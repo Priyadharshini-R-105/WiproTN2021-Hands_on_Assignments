bfe0bc5397ad88412b386d2e441d5e98fe7fe101
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-07-16 18:59:07 +0530

    Create Odd or even.java

---
+++ FlowControl/Odd or even.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class OddEven
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("EVEN");
+}
+else
+{
+System.out.println("ODD");
+}
+}
+
+
+//N=2
+
+EVEN
