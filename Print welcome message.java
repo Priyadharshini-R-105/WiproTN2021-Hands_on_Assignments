9d82d14ef40a0b7fa48c450913420e3f99ff3391
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-07-16 18:27:39 +0530

    Create Print welcome message.java

---
+++ Print welcome message.java
@@ -0,0 +1,17 @@
+public class Welcome
+{
+public static void main(String []args);
+{
+System.out.println("Welcome"+args[0]);
+}
+}
+
+//During compile:
+
+java Welcome.java
+java Welcome John
+
+//Output
+
+Welcome john
+
