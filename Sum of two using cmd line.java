f761b40cac7f45d7509a2960d22cd29a63119166
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-07-16 18:30:25 +0530

    Create Sum of two using cmd line.java

---
+++ Sum of two using cmd line.java
@@ -0,0 +1,16 @@
+Class sum
+{
+public static void main(String[] args)
+{
+System.out.println("The sum of args[0] and args[1] is");
+}
+}
+
+//Compilation
+
+java sum.java
+java sum 10 20 
+
+//output
+
+The sum of 10 and 20 is 30
