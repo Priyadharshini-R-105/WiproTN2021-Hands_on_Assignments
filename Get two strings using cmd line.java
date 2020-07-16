b211d2654c3b3d197a04fcca36e6e4bb1bb0c535
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-07-16 18:21:43 +0530

    Update Get two strings using cmd line.java

--- Get two strings using cmd line.java
+++ Get two strings using cmd line.java
@@ -5,3 +5,10 @@ public static void main(String []args)
 System.out.println(args[0]+"technologies"+args[1]);
 }
 }
+
+//while executing 
+java main wipro,bangalore,abc,mumbai
+//Output will be
+
+Wipro technologies bangalore
+ABC technologied mumbai
